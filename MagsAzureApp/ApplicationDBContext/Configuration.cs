5bdd526ddcc8e2e5bedea7e361f6df14ddfe81b4
magda <adesign@spray.se>
2015-06-11 09:31:51 +0200

    remover user

--- MagsAzureApp/ApplicationDBContext/Configuration.cs
+++ MagsAzureApp/ApplicationDBContext/Configuration.cs
@@ -34,50 +34,14 @@ namespace MagsAzureApp.ApplicationDBContext
             //create a dummy user
             var user = new ApplicationUser()
             {
-                UserName = "user1@contoso.com",
-                Email = "user1@contoso.com"
+                UserName = "dummy@dummy.com",
+                Email = "dummy@dummy.com"
             };
 
-            identityResult = userManager.Create(user, "P_assw0rd1");
+            identityResult = userManager.Create(user, "password");
             //set the users role to canEdit
             if (identityResult.Succeeded == true)
                 identityResult = userManager.AddToRole(user.Id, "canEdit");
-        }
-        bool AddUserAndRole(MagsAzureApp.Models.ApplicationDbContext context)
-        {
-            IdentityResult identityResult;
-            var roleManager = new RoleManager<IdentityRole>
-                (new RoleStore<IdentityRole>(context));
-            identityResult = roleManager.Create(new IdentityRole("canEdit"));
-            var userManager = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(context));
-            var user = new ApplicationUser()
-            {
-                UserName = "admin1@admin.com",
-                Email = "admin1@admin.com"
-            };
-            identityResult = userManager.Create(user, "_Password1234_");
-            if (identityResult.Succeeded == false)
-                return identityResult.Succeeded;
-            identityResult = userManager.AddToRole(user.Id, "canEdit");
-            var user2 = new ApplicationUser()
-            {
-                UserName = "user1@contoso.com",
-            };
-            identityResult = userManager.Create(user2, "_Password1234_");
-            if (identityResult.Succeeded == false)
-                return identityResult.Succeeded;
-            identityResult = userManager.AddToRole(user2.Id, "canEdit");
-
-            var user3 = new ApplicationUser()
-            {
-                UserName = "user2@contoso.se",
-            };
-            identityResult = userManager.Create(user2, "P_assw0rd3");
-            if (identityResult.Succeeded == false)
-                return identityResult.Succeeded;
-            identityResult = userManager.AddToRole(user3.Id, "canEdit");
-            return identityResult.Succeeded;
         }
     }
 
